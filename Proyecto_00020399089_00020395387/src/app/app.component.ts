b75b6fd4e795b9c2575585dc97ae08bb4f4d452d
juan pablo sanchez <juansanchezg@javeriana.edu.co>
2022-02-28 20:43:11 -0500

    creando el proyecto

---
+++ Proyecto_00020399089_00020395387/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Proyecto_00020399089_00020395387';
+}
