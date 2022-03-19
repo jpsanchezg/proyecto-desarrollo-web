7024a97bc300701e6db7932f2b37600ad08e861a
juan pablo sanchez <juansanchezg@javeriana.edu.co>
2022-03-19 00:10:24 -0500

    vista login

--- proyecto/src/app/login-user/login-user.component.ts
+++ proyecto/src/app/login-user/login-user.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-user',
+
   templateUrl: './login-user.component.html',
+
   styleUrls: ['./login-user.component.css']
 })
 export class LoginUserComponent implements OnInit {
